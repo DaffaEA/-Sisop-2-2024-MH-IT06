29bc57d430856f06ad18908cbf09cf56d76a74d2
nessorrowz <142997842+nessorrowz@users.noreply.github.com>
2024-04-27 22:00:11 +0700

    Update setup.c

--- soal_4/setup.c
+++ soal_4/setup.c
@@ -1,133 +1,170 @@
+#include <sys/types.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <sys/types.h>
-#include <sys/wait.h>
+#include <signal.h>
 
-#define MAX_APPS 50
-#define MAX_NAME_LENGTH 50
+#define MAX_APPS 10
+#define MAX_APP_NAME 50
+#define FILENAME "running_apps.txt"
 
-typedef struct {
-    char name[MAX_NAME_LENGTH];
-    int num_windows;
-} AppInfo;
+pid_t running_apps[MAX_APPS];
+int num_running_apps = 0;
 
-void open_app(const char *app_name, int num_windows) {
-    for (int i = 0; i < num_windows; ++i) {
-        pid_t pid = fork();
-        if (pid == 0) {
-            // Child process
-            if (execlp(app_name, app_name, NULL) == -1) {
-                perror("Error opening application");
-                exit(EXIT_FAILURE);
+void addRunningApp(pid_t pid) {
+    if (num_running_apps < MAX_APPS) {
+        running_apps[num_running_apps++] = pid;
+        printf("Jumlah aplikasi berjalan: %d\n", num_running_apps);
+    }
+}
+
+void loadRunningApps() {
+    FILE *file = fopen(FILENAME, "r");
+    if (file != NULL) {
+        fscanf(file, "%d", &num_running_apps);
+        for (int i = 0; i < num_running_apps; i++) {
+            fscanf(file, "%d", &running_apps[i]);
+        }
+        fclose(file);
+    }
+}
+
+void saveRunningApps() {
+    FILE *file = fopen(FILENAME, "w");
+    if (file != NULL) {
+        fprintf(file, "%d\n", num_running_apps);
+        for (int i = 0; i < num_running_apps; i++) {
+            fprintf(file, "%d\n", running_apps[i]);
+        }
+        fclose(file);
+    }
+}
+
+void openApps(char *apps[], int num_apps[]) {
+    int i, j;
+    pid_t pid;
+
+    for (i = 0; i < MAX_APPS && apps[i] != NULL; i++) {
+        for (j = 0; j < num_apps[i]; j++) {
+            pid = fork();
+
+            if (pid == 0) {
+                execlp(apps[i], apps[i], NULL);
+                exit(0);
+            } else {
+                addRunningApp(pid);
             }
-        } else if (pid < 0) {
-            perror("Error forking process");
-            exit(EXIT_FAILURE);
         }
     }
 }
 
-void kill_all_apps() {
-    execlp("pkill", "pkill", "-f", "setup", NULL);
-    perror("Error killing applications");
-    exit(EXIT_FAILURE);
+void closeApp(char *app) {
+    if (fork() == 0) {
+        execlp("pkill", "pkill", app, NULL);
+        exit(0);
+    }
+    sleep(1);
 }
 
-void kill_apps_from_file(const char *filename) {
+void closeAppsFromFile(const char* filename) {
+    FILE *file = fopen(filename, "r");
+    if (file != NULL) {
+        char app[256];
+        int num;
+        while (fscanf(file, "%s %d", app, &num) == 2) {
+            closeApp(app);
+        }
+        fclose(file);
+    }
+}
+
+void readConfigFile(char *filename) {
     FILE *file = fopen(filename, "r");
     if (file == NULL) {
-        perror("Error opening configuration file");
-        exit(EXIT_FAILURE);
+        printf("Error: Cannot open file %s\n", filename);
+        exit(1);
     }
 
-    char app_name[MAX_NAME_LENGTH];
-    int num_windows;
+    char line[MAX_APP_NAME];
+    char *app;
+    int num;
 
-    while (fscanf(file, "%s %d", app_name, &num_windows) == 2) {
-        pid_t pid = fork();
-        if (pid == 0) {
-            // Child process
-            if (execlp("pkill", "pkill", "-f", app_name, NULL) == -1) {
-                perror("Error killing application");
-                exit(EXIT_FAILURE);
+    while (fgets(line, sizeof(line), file)) {
+        app = strtok(line, " ");
+        num = atoi(strtok(NULL, " \n"));
+
+        for (int i = 0; i < num; i++) {
+            pid_t pid = fork();
+            if (pid == 0) {
+                execlp(app, app, NULL);
+                exit(0);
+            } else {
+                addRunningApp(pid);
             }
-        } else if (pid < 0) {
-            perror("Error forking process");
-            exit(EXIT_FAILURE);
         }
     }
 
     fclose(file);
 }
 
-void handle_open(int argc, char *argv[]) {
-    if (argc < 4 || (argc - 2) % 2 != 0) {
-        printf("Usage: ./setup -o <app1> <num1> <app2> <num2> ... <appN> <numN>\n");
-        exit(EXIT_FAILURE);
+void closeApps() {
+    FILE *file = fopen(FILENAME, "r");
+    if (file != NULL) {
+        int num_apps;
+        fscanf(file, "%d", &num_apps);
+        for (int i = 0; i < num_apps; i++) {
+            int pid;
+            fscanf(file, "%d", &pid);
+            if (kill(pid, SIGTERM) == -1) {
+                perror("Error killing process");
+            }
+        }
+        fclose(file);
     }
-
-    int num_apps = (argc - 2) / 2;
-    AppInfo apps[MAX_APPS];
-
-    for (int i = 0; i < num_apps; ++i) {
-        strcpy(apps[i].name, argv[2 + i * 2]);
-        apps[i].num_windows = atoi(argv[3 + i * 2]);
-    }
-
-    for (int i = 0; i < num_apps; ++i) {
-        open_app(apps[i].name, apps[i].num_windows);
-    }
-}
-
-void handle_from_file(const char *filename) {
-    kill_all_apps(); 
-
-    FILE *file = fopen(filename, "r");
-    if (file == NULL) {
-        perror("Error opening configuration file");
-        exit(EXIT_FAILURE);
-    }
-
-    char app_name[MAX_NAME_LENGTH];
-    int num_windows;
-
-    while (fscanf(file, "%s %d", app_name, &num_windows) == 2) {
-        open_app(app_name, num_windows);
-    }
-
-    fclose(file);
 }
 
 int main(int argc, char *argv[]) {
+    loadRunningApps();
+
     if (argc < 2) {
-        printf("Usage: ./setup [-o <app1> <num1> <app2> <num2> ... <appN> <numN>] | [-f <filename>] | [-k | -k <filename>]\n");
-        return EXIT_FAILURE;
+        printf("Penggunaan: %s -o <app1> <num1> <app2> <num2> ... <appN> <numN> or %s -f <filename> or %s -k\n", argv[0], argv[0], argv[0]);
+        exit(1);
     }
 
     if (strcmp(argv[1], "-o") == 0) {
-        handle_open(argc, argv);
+        char *apps[MAX_APPS];
+        int num_apps[MAX_APPS];
+        int numArgs = argc - 3;
+
+        int i, j, appIndex = 0;
+        for (i = 2; i < argc; i += 2) {
+            apps[appIndex] = argv[i];
+            num_apps[appIndex] = atoi(argv[i + 1]);
+            appIndex++;
+        }
+        apps[appIndex] = NULL;
+
+        openApps(apps, num_apps);
+        saveRunningApps();
     } else if (strcmp(argv[1], "-f") == 0) {
         if (argc != 3) {
-            printf("Usage: ./setup -f <filename>\n");
-            return EXIT_FAILURE;
+            printf("Penggunaan: %s -f <filename>\n", argv[0]);
+            exit(1);
         }
-        handle_from_file(argv[2]);
+        readConfigFile(argv[2]);
+        saveRunningApps();
     } else if (strcmp(argv[1], "-k") == 0) {
-        if (argc == 2) {
-            kill_all_apps();
-        } else if (argc == 3) {
-            handle_from_file(argv[2]);
+        if (argc == 3) {
+            closeAppsFromFile(argv[2]);
         } else {
-            printf("Usage: ./setup -k | -k <filename>\n");
-            return EXIT_FAILURE;
+            closeApps();
+            remove(FILENAME);
         }
     } else {
-        printf("Invalid option\n");
-        return EXIT_FAILURE;
+        printf("Opsi tidak valid\n");
+        exit(1);
     }
 
-    return EXIT_SUCCESS;
+    return 0;
 }
-
